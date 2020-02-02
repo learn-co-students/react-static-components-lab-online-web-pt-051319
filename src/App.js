8a477f1db654eb65ac41203f8adc5d20aa8b258d
jasmine1226 <jasmine1226@gmail.com>
2020-02-01 18:31:10 -0800

    Done.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import CatComponent from "./CatComponent";
+import MouseComponent from "./MouseComponent";
+import EinsteinQuoteComponent from "./EinsteinQuoteComponent";
 
 class App extends Component {
-	render() {
-		// your code in the return statement below!
-		return (
-			<div className="App">
-				<CatComponent />
-				<EinsteinQuoteComponent />
-				{/* one more component missing */}
-			</div>
-		);
-	}
+  render() {
+    // your code in the return statement below!
+    return (
+      <div className="App">
+        <CatComponent />
+        <EinsteinQuoteComponent />
+        <MouseComponent />
+      </div>
+    );
+  }
 }
 
 export default App;
